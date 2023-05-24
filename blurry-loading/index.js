25888c1475f0d28c230e37613cbcef193ffe7c4b
Randy Verduguez <rverduguez@corp.dharbor.com>
2023-05-23 22:46:37 -0400

    completed blurry loading

---
+++ blurry-loading/index.js
@@ -0,0 +1,21 @@
+const loadingText = document.querySelector(".loading-text");
+const background = document.querySelector(".background");
+
+let load = 0;
+let interval = setInterval(blurring, 30);
+
+function blurring() {
+  load++;
+
+  if (load > 99) {
+    clearInterval(interval);
+  }
+
+  loadingText.innerText = `${load}%`;
+  loadingText.style.opacity = scale(load, 0, 100, 1, 0);
+  background.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
+
+function scale(num, in_min, in_max, out_min, out_max) {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+}
